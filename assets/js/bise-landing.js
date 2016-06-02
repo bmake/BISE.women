1e7284f3de3f2ad10259ea6c8763a7e506ca7ef1
Jonas <jetschni@gmail.com>
2016-06-02 23:10:29 +0200

    min files added

--- assets/js/bise-landing.js
+++ assets/js/bise-landing.js
@@ -1,11 +1,18 @@
-$('.carousel').carousel({
-    interval: 0
-});
 
-$( "#call-to-action-question" ).click(function() {
-    $( "sk-wrapper" ).trigger( "click" );
-});
 
-$("img.lazy").lazyload({
+$(function() {
+
+    $('.carousel').carousel({
+        interval: 0
+    });
+
+    $( "#call-to-action-question" ).click(function() {
+        $( "sk-wrapper" ).trigger( "click" );
+    });
+
+    $("img.lazy").lazyload({
+        effect : "fadeIn",
+        threshold : 200
+    });
 
 });
