fec59855663d903667734ab7d4b87bb768ebce8d
Jonas <jetschni@gmail.com>
2016-06-02 23:37:25 +0200

    Lazyload removed

--- assets/js/bise-landing.js
+++ assets/js/bise-landing.js
@@ -1,17 +1,17 @@
 $(function() {
-
-
+    /*
     $("img.lazy").lazyload({
         effect : "fadeIn",
         threshold : 200
-    });
+    });*/
 
     $('.carousel').carousel({
         interval: 0
     });
 
+    /*
     $( "#call-to-action-question" ).click(function() {
         $( "sk-wrapper" ).trigger( "click" );
-    });
+    });*/
 
 });
